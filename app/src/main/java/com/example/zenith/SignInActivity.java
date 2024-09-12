3d8f029b1a58285602ff5a461c088a4dcc5c976f
Mesherekov <plof0676@gmail.com>
2024-09-12 17:33:23 +0600

    Firebase Auth

--- app/src/main/java/com/example/zenith/SignInActivity.java
+++ app/src/main/java/com/example/zenith/SignInActivity.java
@@ -1,14 +1,54 @@
 package com.example.zenith;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private FirebaseAuth mfirebaseAuth;
+    private EditText edemail, edpassword;
+    private Button bsignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        init();
+        bsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(edemail.getText().toString()) && !TextUtils.isEmpty(edpassword.getText().toString())) {
+                    mfirebaseAuth.signInWithEmailAndPassword(edemail.getText().toString(), edpassword.getText().toString()).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                FirebaseUser currentuser = mfirebaseAuth.getCurrentUser();
+                                assert currentuser != null;
+                                Toast.makeText(SignInActivity.this, currentuser.getEmail(), Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                                Toast.makeText(SignInActivity.this, "Sign In failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else Toast.makeText(SignInActivity.this, "Please fill in the fields", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void init(){
+        edemail = findViewById(R.id.emailin);
+        edpassword = findViewById(R.id.passin);
+        bsignin = findViewById(R.id.enterac);
+        mfirebaseAuth = FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/example/zenith/SignUpActivity.java
+++ app/src/main/java/com/example/zenith/SignUpActivity.java
@@ -1,14 +1,67 @@
 package com.example.zenith;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private FirebaseAuth mfirebaseAuth;
+    private EditText edemail, edname, edpassword;
+    private Button bsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        init();
+        bsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(edname.getText().toString()) && !TextUtils.isEmpty(edemail.getText().toString()) && !TextUtils.isEmpty(edpassword.getText().toString())){
+                    mfirebaseAuth.createUserWithEmailAndPassword(edemail.getText().toString(), edpassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "User Sign Up", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "User Sign Up Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Please fill in the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentuser = mfirebaseAuth.getCurrentUser();
+        if(currentuser != null){
+            Toast.makeText(this, "User not null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void init(){
+        edemail = findViewById(R.id.emailup);
+        edname = findViewById(R.id.nameup);
+        edpassword = findViewById(R.id.passup);
+        bsignup = findViewById(R.id.createaccount);
+        mfirebaseAuth = FirebaseAuth.getInstance();
     }
 }
