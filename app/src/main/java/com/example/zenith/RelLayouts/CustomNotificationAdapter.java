92b1c204d200dd5dffb71f354bc5b6cf668b40ac
Mesherekov <plof0676@gmail.com>
2024-12-14 21:31:41 +0600

    Notification RelativeLayout 2

--- app/src/main/java/com/example/zenith/RelLayouts/CustomNotificationAdapter.java
+++ app/src/main/java/com/example/zenith/RelLayouts/CustomNotificationAdapter.java
@@ -1,6 +1,10 @@
 package com.example.zenith.RelLayouts;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -9,10 +13,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.zenith.ItemNotification;
 import com.example.zenith.R;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
-public class CustomNotificationAdapter extends RecyclerView.Adapter {
+public class CustomNotificationAdapter extends RecyclerView.Adapter<CustomNotificationViewHolder> {
     Context context;
     List<ItemNotification> itemNotifications;
 
@@ -23,13 +29,33 @@ public class CustomNotificationAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CustomNotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new CustomNotificationViewHolder(LayoutInflater.from(context).inflate(R.layout.item_notification, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CustomNotificationViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.name.setTextColor(itemNotifications.get(position).getColortext());
+        holder.name.setText(itemNotifications.get(position).getName());
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Drawable drawable = new BitmapDrawable(context.getResources(), bitmap);
+                holder.imageView.setImageDrawable(drawable);
+                itemNotifications.get(position).setImage(holder.imageView.getDrawable());
+            }
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
 
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+        holder.imageView.setTag(target);
+        Picasso.get().load(itemNotifications.get(position).getUri()).resize(400, 400).centerCrop().placeholder(R.drawable.profileicon).into(target);
     }
 
     @Override
